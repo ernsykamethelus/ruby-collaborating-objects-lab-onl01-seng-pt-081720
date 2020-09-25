f7e6d599c70a7ada51470cd269242211b8ad11d0
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-25 02:12:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,6 +18,7 @@ end
   def self.new_by_filename(filename)
     split_name = filename.split (" - ")
     song = self.new(split_name[1])
+    save
     artist = Artist.find_or_create_by_name(split_name[0])
     song.artist = artist
     song
